6cc2e742cf8b9d726d5ae23f00816e83960ae460
Tanya Kravchenko <mail-tanja@mail.ru>
2021-08-22 00:05:33 +0300

    Created app-reducer

--- src/Redux/Reducers/app-reducer.ts
+++ src/Redux/Reducers/app-reducer.ts
@@ -1,23 +1,36 @@
-type InitialStateType = {
-}
-
-type ActionsType = {}
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
 const initialState = {
-
+    status: 'succeeded' as RequestStatusType,
+    error: null as string | null,
+    isInitialized: false
 }
 
-export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
-    switch (action) {
-        case '':
-            return {
-                ...state
+export type AppInitialStateType = typeof initialState
 
-            }
+export const appReducer = (state: AppInitialStateType = initialState, action: AppActionsType): AppInitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
+
+        case "APP/SET-IS-INITIALIZED":
+            return {...state, isInitialized: action.isInitialized}
         default:
             return state
     }
 }
+
+export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
+export const setIsInitializedAC = (isInitialized: boolean) => ({type: 'APP/SET-IS-INITIALIZED', isInitialized} as const)
+
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
+export type SetIsInitializedAC = ReturnType<typeof setIsInitializedAC>
+
+export type AppActionsType = SetAppStatusActionType | SetAppErrorActionType | SetIsInitializedAC
 
 
 
