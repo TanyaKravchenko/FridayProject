3614639859ca7e24742baed3b90206328adbb96f
TrocyukS96 <trotzuk.stanislav@gmail.com>
2021-12-11 11:52:49 +0300

    added styles for login page

--- src/n2-features/Login/Login.tsx
+++ src/n2-features/Login/Login.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import s from './Login.module.css';
+import s from './Login.module.scss';
+import InputText from "../super components/InputText/InputText";
+import Button from "../super components/Button/Button";
 
-type LoginPropsType = {
-}
+type LoginPropsType = {}
 
 export const Login: React.FC<LoginPropsType> = () => {
     return (
         <div className={s.loginBlock}>
-            Login
+            <h2 className={s.logo}>It-incubator</h2>
+            <h3 className={s.title}>Sign in</h3>
+            <div className={s.emailBlock}>
+                <span>Email</span>
+                <InputText type={'text'} className={s.inputText}/>
+            </div>
+            <div className={s.passwordBlock}>
+                <span>Password</span>
+                <input type={'password'} className={s.inputPassword}/>
+            </div>
+            <a href="#" className={s.link}>Forgot password</a>
+            <button className={s.loginBtn}> Login</button>
+            <div className={s.redirectBlock}>
+                <span className={s.redirectSpan}>Don’t have an account?</span>
+                <button className={s.signBtn}>Sign Up</button>
+            </div>
+
         </div>
     )
 }
