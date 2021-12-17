3ebc1b286e5c3f607747ee287790617b8f59216e
Tanya Kravchenko <mail-tanja@mail.ru>
2021-12-17 10:07:38 +0300

    navbar+

--- src/n1-main/m1-ui/header/Navbar/Navbar.tsx
+++ src/n1-main/m1-ui/header/Navbar/Navbar.tsx
@@ -7,29 +7,31 @@ type NavbarPropsType = {}
 
 const Navbar: React.FC<NavbarPropsType> = () => {
     return (
-        <div className={s.navLinkItems}>
-            <div className={s.item}>
-                <NavLink to={path.ALL_COMPONENTS} className={s.navLink}
-                         activeClassName={s.active}> Main </NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to={path.PROFILE} className={s.navLink}
-                         activeClassName={s.active}> Profile </NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to={path.LOGIN} className={s.navLink} activeClassName={s.active}> Login </NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to={path.REG} className={s.navLink}
-                         activeClassName={s.active}> Registration </NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to={path.PASS_REC} className={s.navLink}
-                         activeClassName={s.active}> PasswordRecovery </NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to={path.PASSWORD} className={s.navLink}
-                         activeClassName={s.active}>  NewPassword </NavLink>
+        <div>
+            <div className={s.navLinkItems}>
+                <div className={s.item}>
+                    <NavLink to={path.ALL_COMPONENTS} className={s.navLink}
+                             activeClassName={s.active}> Main </NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to={path.PROFILE} className={s.navLink}
+                             activeClassName={s.active}> Profile </NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to={path.LOGIN} className={s.navLink} activeClassName={s.active}> Login </NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to={path.REG} className={s.navLink}
+                             activeClassName={s.active}> Registration </NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to={path.PASS_REC} className={s.navLink}
+                             activeClassName={s.active}> PasswordRecovery </NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to={path.PASSWORD} className={s.navLink}
+                             activeClassName={s.active}> NewPassword </NavLink>
+                </div>
             </div>
         </div>
     );
