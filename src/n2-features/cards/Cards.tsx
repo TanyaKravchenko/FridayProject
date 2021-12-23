dc47e763ca55e8c84dd486258fe9d4e5ee31bb1b
Tanya Kravchenko <mail-tanja@mail.ru>
2021-12-23 23:59:05 +0300

    +

--- src/n2-features/cards/Cards.tsx
+++ src/n2-features/cards/Cards.tsx
@@ -1,24 +1,24 @@
-import React from "react";
+import React from 'react';
 import s from './Cards.module.scss';
 import arrow from './../../assets/images/icons/arrow-icon.png'
-import {useDispatch, useSelector} from "react-redux";
-import {RootStateType} from "../../n1-main/m2-bll/store";
+import {useDispatch, useSelector} from 'react-redux';
+import {RootStateType} from '../../n1-main/m2-bll/store';
 import {NavLink} from 'react-router-dom';
-import {path} from "../../n1-main/m1-ui/routes/Routes";
-import {AddCard} from "./addCard/AddCard";
-import {useParams} from "react-router";
-import {OneCardType} from "../../n1-main/m3-dal/cards-api";
-import {deleteCardTC} from "../../n1-main/m2-bll/reducers/cards-reducer";
+import {path} from '../../n1-main/m1-ui/routes/Routes';
+import {AddCard} from './addCard/AddCard';
+import {OneCardType} from '../../n1-main/m3-dal/cards-api';
+import {deleteCardTC} from '../../n1-main/m2-bll/reducers/cards-reducer';
 
 export const Cards = () => {
     // HOOKS
     const cards = useSelector<RootStateType, OneCardType[]>(state => state.cards.cards)
     const dispatch = useDispatch()
-    const packId = useSelector<RootStateType, string>(state=>state.cards.packId)
+    const packId = useSelector<RootStateType, string>(state => state.cards.packId)
     // HANDLERS
-    const handleDeleteCard = (packID:string, cardID:string) => {
+    const handleDeleteCard = (packID: string, cardID: string) => {
         dispatch(deleteCardTC(packID, cardID))
     }
+
     return (
 
         <div className={s.cards}>
@@ -31,7 +31,7 @@ export const Cards = () => {
                 </div>
                 <div className={s.btnBox}>
                     <div className={s.inputWrap}>
-                        <input className={s.cardsInput} placeholder={"Search..."}/>
+                        <input className={s.cardsInput} placeholder={'Search...'}/>
                     </div>
                 </div>
 
@@ -60,7 +60,8 @@ export const Cards = () => {
                                         {card.grade}
                                     </div>
                                     <div className={s.cardsRowItem}>
-                                        <button className={s.deleteBtn} onClick={() => handleDeleteCard(card.cardsPack_id, card._id)}>delete
+                                        <button className={s.deleteBtn}
+                                                onClick={() => handleDeleteCard(card.cardsPack_id, card._id)}>delete
                                         </button>
                                         <button className={s.editBtn}>edit</button>
                                     </div>
